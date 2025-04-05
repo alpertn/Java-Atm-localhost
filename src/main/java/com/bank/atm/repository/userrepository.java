b1ddbafa5457114cd8f08fc5b6ca0c9d690e2886
alpertn <ozbaymehmetalper@gmail.com>
2025-04-05 23:24:23 +0300

    userrepository güncellemesi.

--- src/main/java/com/bank/atm/repository/userrepository.java
+++ src/main/java/com/bank/atm/repository/userrepository.java
@@ -16,4 +16,16 @@ public class userrepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
+    //Rowmapper sql'den gelen verileri cevirmeye yarar.
+    private final RowMapper<User> rowMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.SetId(rs.getLong("id")); // id degiskeni LONG olarak alınıyor.
+        user.SetName(rs.getString("name"));
+        user.SetSurname(rs.getString("surname"));
+        user.SetTckimlikno(rs.getString("tckimlikno"));
+        user.SetBirthdate(rs.getDate("birth_date").toLocalDate());
+        return user;
+    };
+
 }
