344d887e485d4b3f8cf0e2439169c3b1df35f391
alpertn <ozbaymehmetalper@gmail.com>
2025-04-06 05:58:51 +0300

    userrepository güncellemesi.

--- src/main/java/com/bank/atm/models/User.java
+++ src/main/java/com/bank/atm/models/User.java
@@ -21,6 +21,7 @@ public class User {
     }
 
     public Long getId(){
+
         return id;
     }
     public void SetId(Long id){
--- src/main/java/com/bank/atm/repository/userrepository.java
+++ src/main/java/com/bank/atm/repository/userrepository.java
@@ -10,17 +10,17 @@ import java.util.Optional;
 @Repository
 public class userrepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate; // databaseconfig.java kodundaki sql'e baglanır. sql ile ilgili olan şeyleri burdan yapabılırız.
 
     public userrepository(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
 
 
-    //Rowmapper sql'den gelen verileri cevirmeye yarar.
+    // Rowmapper sql'den gelen verileri cevirmeye yarar.
     private final RowMapper<User> rowMapper = (rs, rowNum) -> {
-        User user = new User();
-        user.SetId(rs.getLong("id")); // id degiskeni LONG olarak alınıyor.
+        User user = new User(); // user türünde
+        user.SetId(rs.getLong("id")); // sqlde kayıtlı olan id degiskenini LONG olarak alınıyor.
         user.SetName(rs.getString("name"));
         user.SetSurname(rs.getString("surname"));
         user.SetTckimlikno(rs.getString("tckimlikno"));
@@ -28,12 +28,19 @@ public class userrepository {
         return user;
     };
 
+
+    // User türünde bir modül olusturduk. Save modülü User tipinde veri alıyor. sql stringi hazırladık. jdbctemplate ile ? yazılan yerlere user değişkeninin icindekileri aktardık.
     public User Save(User user){
         String sql = "INSERT INTO users (name, surname, tckimlikno, birthdate) VALUES (?,?,?,?)";
+
         jdbcTemplate.update(sql, user.getName(),user.GetSurname(), user.getTckimlikno(), user.getBirthdate());
         return user;
     }
 
 
+    public Optional<User> findbyid(Long gelenid){
+        String sql = "SELECT * FROM users WHERE ID = ?";
+        return jdbcTemplate.query(sql, rowMapper, gelenid).stream().findFirst(); // .stream() kullanmayınca .findFirst() gibi methodları kullanamıyoruz. sql kodunu gönderiyor. veriyi rowmapper'e kaydediyor. ? diye bıraktıgımızı da id ile degistiriyor.
+    }
 
 }
